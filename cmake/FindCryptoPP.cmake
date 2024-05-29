966b975557eef9aa0e394a9e8fd8bc79e247dc5d
Logan Grosz <logan.grosz@gmail.com>
2024-05-29 14:32:16 -0600

    Use _VERSION instead of _VERSION_STRING
    _VERSION_STRING is set as a cached variable for compatibility.

--- cmake/FindCryptoPP.cmake
+++ cmake/FindCryptoPP.cmake
@@ -9,6 +9,8 @@ Output:
 CryptoPP::CryptoPP   - target (release build)
 CryptoPP_INCLUDE_DIR - includes directory containing subdirectory cryptopp/
 CryptoPP_LIBRARY     - CryptoPP library file (libcryptopp.a)
+CryptoPP_VERSION     - The version of the library
+CryptoPP_VERSION_STRING - The version of the library (for compatability, use CryptoPP_VERSION instead)
 ]]
 
 if (CryptoPP_ROOT_DIR)
@@ -43,14 +45,14 @@ endif(CryptoPP_ROOT_DIR)
 if(CryptoPP_INCLUDE_DIR)
     file(STRINGS ${CryptoPP_INCLUDE_DIR}/cryptopp/config.h _config_version REGEX "CRYPTOPP_VERSION")
     string(REGEX MATCH "([0-9]+)([0-9]+)([0-9]+)" _match_version ${_config_version})
-    set(CryptoPP_VERSION_STRING "${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
+    set(CryptoPP_VERSION "${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CryptoPP
     REQUIRED_VARS CryptoPP_INCLUDE_DIR CryptoPP_LIBRARY
     FOUND_VAR CryptoPP_FOUND
-    VERSION_VAR CryptoPP_VERSION_STRING)
+    VERSION_VAR CryptoPP_VERSION)
 
 if(CryptoPP_FOUND AND NOT TARGET CryptoPP::CryptoPP)
     add_library(CryptoPP::CryptoPP UNKNOWN IMPORTED)
@@ -62,4 +64,5 @@ endif()
 mark_as_advanced(CryptoPP_INCLUDE_DIR CryptoPP_LIBRARY)
 set(CryptoPP_INCLUDE_DIRS ${CryptoPP_INCLUDE_DIR})
 set(CryptoPP_LIBRARIES ${CryptoPP_LIBRARY})
+set(CryptoPP_VERSION_STRING ${CryptoPP_VERSION})
 
